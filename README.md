8b721fd96a8632555b7e53c21991a2db2abdba28
patrick <patrick@macbook.local>
2015-11-11 13:15:15 -0500

    Added slave server option

--- README.md
+++ README.md
@@ -4,7 +4,38 @@ dockerenv_mysql
 dockerenv mysql
 
 ### Tags
-* latest
-  * mariadb 5.5 
-* mysql 5.1
-  * mysql 5.1
+Available tags listed here: https://hub.docker.com/r/avatarnewyork/dockerenv-mysql/tags/
+
+### Slave Option (currently only available in tag: percona56)
+You can setup a slave server given the following conditions:
+
+* You must be starting with a blank database (both master and slave).  If you have existing data, mysqldump and then import it after the new master/slave setup.
+* you must be using percona56
+* the master db name is db in the yml file and the slave can be anything else.
+* you must set the following environment varibles:
+  * Master DB
+    * SERVER_ID: 1
+  * Slave DB
+    * SERVER_ID: 2
+    * SLAVE: 1
+
+#### Example yaml
+Really only need to pay attention to environment variables.
+
+```yaml
+db:
+  build: .
+  ports:
+    - "3366:3306"
+  environment:
+    SERVER_ID: 1
+db2:
+  build: .
+  ports:
+    - "3666:3306"
+  links:
+    - db
+  environment:
+    SERVER_ID: 2
+    SLAVE: 1
+```
--- mysql_startup.sh
+++ mysql_startup.sh
@@ -17,4 +17,30 @@ if [ ! -f /var/lib/mysql/ibdata1 ]; then
     sleep 10s
 fi
 
-/usr/sbin/mysqld --user=mysql --max_allowed_packet=250M
+[ -z "$SERVER_ID" ] && SERVER_ID=1
+
+if env | grep -q ^SERVER_ID=
+then
+    echo env variable is already exported
+else
+    echo env variable was not exported, but now it is
+    export SERVER_ID
+fi
+
+if env | grep -q ^SLAVE=
+then
+    START_SLAVE="CHANGE MASTER TO MASTER_HOST='${DB_1_PORT_3306_TCP_ADDR}', MASTER_USER='repl', MASTER_PASSWORD='repl', MASTER_PORT=${DB_1_PORT_3306_TCP_PORT}, MASTER_LOG_FILE='mysql-bin.000001', MASTER_LOG_POS=120, MASTER_CONNECT_RETRY=10; start slave;"
+    export START_SLAVE
+    
+    /usr/sbin/mysqld --user=mysql --max_allowed_packet=250M --server_id=${SERVER_ID} --log_bin=mysql-bin --utility-user='repl@%' --utility-user-privileges='replication slave' --utility_user_password='repl' &
+    sleep 5s
+    mysql -uroot -h127.0.0.1 -e "${START_SLAVE}" 
+    echo $START_SLAVE
+    killall mysqld
+    sleep 3s;
+fi
+
+
+
+/usr/sbin/mysqld --user=mysql --max_allowed_packet=250M --server_id=${SERVER_ID} --log_bin=mysql-bin --utility-user='repl@%' --utility-user-privileges='replication slave' --utility_user_password='repl';
+
